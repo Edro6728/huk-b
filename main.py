d4fb31e9e0e840fed27e7c378abeeecfb8b9bfca
Edro6728 <100054872+Edro6728@users.noreply.github.com>
2022-04-22 16:22:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -241,7 +241,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'anonedev'
+        tl_admin_user = 'Edump2101'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -561,9 +561,9 @@ def onmessage(update,bot:ObigramClient):
             #    bot_token = os.environ.get('bot_token')
             #    
                 # set in debug
-            #    api_id = 7386053
-            #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+            #    api_id = 18403498
+            #    api_hash = '2c0b01283b1b67f7087a92cc81cd91f8'
+            #    bot_token = '5391323120:AAEFdVBFXwnjY_A6cpVD_-W8AcT9U60N1C4'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -579,7 +579,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5333009488:AAFyX2xYvBF-rK-OiTovVOzSq8f8Q0ziXGE'
+    bot_token = '5391323120:AAEFdVBFXwnjY_A6cpVD_-W8AcT9U60N1C4'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
